f86429f40d8577dd15312e90d96ed1aa6c48e7b0
kenlinkkkk <38412933+kenlinkkkk@users.noreply.github.com>
2021-03-08 16:35:39 +0700

    update link dangky

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -46,7 +46,8 @@ class HomeController extends Controller
     public function regSubmit(Request $request)
     {
         $data = $request->except('_token');
-        $url = 'http://dangky.mobiedu.vn/dangky/api/register.jsp?sub='. $data['package'];
+        //$url = 'http://dangky.mobiedu.vn/dangky/api/register.jsp?sub='. $data['package'];
+        $url = 'http://dangky.mobiedu.vn/api/register.jsp?sub='. $data['package'];
 
         return Redirect::away($url);
     }
